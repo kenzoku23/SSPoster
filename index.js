f3bb46fb84c4e8ed328dd9373ebbf7071f3556aa
kenzoku23 <tokugawa041020@gmail.com>
2020-07-01 15:58:49 +0900

    SS撮ってDiscordに流すまで

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
+//レンダラプロセス
+//開発用
+const config = require(`config`);
+
 //画面のイベントはここに書く
-var chokidar = require('chokidar');
+const chokidar = require(`chokidar`);
+const {ipcRenderer} = require(`electron`);
 
-const runBtn = document.getElementById('runBtn');
-const currentSSImg = document.getElementById('currentSSImg');
+const runBtn = document.getElementById(`runBtn`);
+const currentSSImg = document.getElementById(`currentSSImg`);
+const runPostDiscord = document.getElementById(`runPostDiscord`);
 
-const pngPattern = /.*png$/;
+const pngPattern = /.*jpg$/;
 
-runBtn.addEventListener('click', () => {
-    chokidar.watch('ファイル名').on('add', path => {
+runBtn.addEventListener(`click`, () => {
+    chokidar.watch(config.filePath, {
+        ignored: `**/thumbnails/**`,
+        ignoreInitial : true
+    }).on(`add`, path => {
         console.log(path);
         if(pngPattern.test(path)){
             currentSSImg.src = path;
         }
     });
+});
+
+runPostDiscord.addEventListener(`click`, () => {
+    ipcRenderer.send(`postImage`, decodeURI(currentSSImg.src.replace(new RegExp(`^${location.origin}`), ``)));
 });
--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-'use strict';
+//メインプロセス
+`use strict`;
+//開発用
+const config = require(`config`);
 
-const electron = require('electron');
+const electron = require(`electron`);
+const {ipcMain} = require(`electron`);
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-app.on('ready', function() {
+const Discord = require(`discord.js`);
+const discordCli = new Discord.Client();
+discordCli.login(config.discordToken);
+
+app.on(`ready`, function() {
     let window = new BrowserWindow({
         width: 800,
         height: 600,
@@ -12,15 +20,24 @@ app.on('ready', function() {
             nodeIntegration: true
         }
     });
-    window.loadURL('file://' + __dirname + '/index.html');
-    window.on('closed', function() {
+    window.loadURL(`file://${__dirname}/index.html`);
+    window.on(`closed`, function() {
         window = null;
     })
 });
 
-app.on('window-all-closed', function() {  
+app.on(`window-all-closed`, function() {  
     //mac OSはユーザが明示的に終了するまでアプリケーションをアクティブにする。
-    if(process.platform !== 'darwin') {
+    if(process.platform !== `darwin`) {
       app.quit();
     }
+    discordCli.destroy();
 });
+
+app.allowRendererProcessReuse = true;
+
+ipcMain.on(`postImage`, (event, arg) => {
+    discordCli.channels.cache.find(ch => ch.name === config.channelName).send({
+        files: [arg]
+    });
+})
