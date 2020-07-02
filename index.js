340760c360dc59603f37b668989b84ff683a19d7
kenzoku23 <tokugawa041020@gmail.com>
2020-07-02 16:16:45 +0900

    SSがすでに表示されている時はimgを更新しないようにしたかった。

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 //レンダラプロセス
 //開発用
-const config = require(`config`);
+const config = require("config");
 
 //画面のイベントはここに書く
-const chokidar = require(`chokidar`);
-const {ipcRenderer} = require(`electron`);
+const chokidar = require("chokidar");
+const {ipcRenderer} = require("electron");
 
-const runBtn = document.getElementById(`runBtn`);
-const currentSSImg = document.getElementById(`currentSSImg`);
-const runPostDiscord = document.getElementById(`runPostDiscord`);
+const runBtn = document.getElementById("runBtn");
+const currentSSImg = document.getElementById("currentSSImg");
+const postDiscordBtn = document.getElementById("postDiscordBtn");
+const notPostBtn = document.getElementById("notPostBtn");
 
-const pngPattern = /.*jpg$/;
 
-runBtn.addEventListener(`click`, () => {
-    chokidar.watch(config.filePath, {
-        ignored: `**/thumbnails/**`,
+runBtn.addEventListener("click", () => {
+    chokidar.watch(`${config.filePath}/**/*.jpg`, {
+        ignored: "**/thumbnails/**",
         ignoreInitial : true
-    }).on(`add`, path => {
-        console.log(path);
-        if(pngPattern.test(path)){
+    }).on("add", path => {
+        //問題ない?
+        console.log(currentSSImg.src);
+        if(!currentSSImg.src){
+            console.log(path);
             currentSSImg.src = path;
         }
     });
 });
 
-runPostDiscord.addEventListener(`click`, () => {
-    ipcRenderer.send(`postImage`, decodeURI(currentSSImg.src.replace(new RegExp(`^${location.origin}`), ``)));
+postDiscordBtn.addEventListener("click", () => {
+    ipcRenderer.send("postImage", decodeURI(new URL(currentSSImg.src).pathname));
 });
+
+notPostBtn.addEventListener("click", () => {
+    //なんかHTMLのパスが入る。なんで
+    currentSSImg.src = "";
+    console.log(currentSSImg.src)
+})
--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 //メインプロセス
-`use strict`;
+"use strict";
 //開発用
-const config = require(`config`);
+const config = require("config");
 
-const electron = require(`electron`);
-const {ipcMain} = require(`electron`);
+const electron = require("electron");
+const {ipcMain} = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-const Discord = require(`discord.js`);
+const Discord = require("discord.js");
 const discordCli = new Discord.Client();
 discordCli.login(config.discordToken);
 
-app.on(`ready`, function() {
+app.on("ready", function() {
     let window = new BrowserWindow({
         width: 800,
         height: 600,
@@ -21,22 +21,21 @@ app.on(`ready`, function() {
         }
     });
     window.loadURL(`file://${__dirname}/index.html`);
-    window.on(`closed`, function() {
+    window.on("closed", function() {
         window = null;
     })
 });
 
-app.on(`window-all-closed`, function() {  
+app.on("window-all-closed", function() {  
     //mac OSはユーザが明示的に終了するまでアプリケーションをアクティブにする。
-    if(process.platform !== `darwin`) {
+    if(process.platform !== "darwin") {
       app.quit();
     }
-    discordCli.destroy();
 });
 
 app.allowRendererProcessReuse = true;
 
-ipcMain.on(`postImage`, (event, arg) => {
+ipcMain.on("postImage", (event, arg) => {
     discordCli.channels.cache.find(ch => ch.name === config.channelName).send({
         files: [arg]
     });
